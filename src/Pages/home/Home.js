531e5f150fcfaaaec2981c7601d8d2803d820e05
ShashankPole <pole.shashank@infokalash.com>
2023-08-21 12:26:14 +0530

    first commit

---
+++ src/Pages/home/Home.js
@@ -0,0 +1,33 @@
+import React from 'react';
+import { Container, CardContent, Typography, CardMedia } from '@material-ui/core';
+
+function MyComponent(props) {
+  const { title, description, showImage, imageUrl, layout, titleStyle, descriptionStyle, imageStyle, imageDirection } = props;
+
+  const containerStyle = {
+    margin: "40px",
+    display: 'flex',
+    flexDirection: layout.flexDirection || 'row',
+    justifyContent: layout.justifyContent || 'space-between',
+    alignItems: layout.alignItems || 'center',
+  };
+
+  const reversedImageStyle = {
+    ...imageStyle,
+    float: imageDirection === 'left' ? 'right' : 'left',
+  };
+
+  return (
+    <Container style={containerStyle}>
+      {showImage && <CardMedia component="img" src={imageUrl} alt="Component Image" style={reversedImageStyle} />}
+      <div>
+        <CardContent>
+          <Typography variant="h6" style={titleStyle}>{title}</Typography>
+          {description && <Typography variant="body2" style={descriptionStyle}>{description}</Typography>}
+        </CardContent>
+      </div>
+    </Container>
+  );
+}
+
+export default MyComponent;
---
+++ src/configurations/componentConfig.js
@@ -0,0 +1,85 @@
+// componentConfig.js
+export const config1 = {
+  title: "Config Driven Component",
+  description:
+    "This is a configurable component body. and god the Linde is a leading global industrial gases and engineering company with 2022 sales of $33 billion. We live our mission of making our world more productive every day by providin",
+  showImage: true,
+  imageUrl: require("../assets/images/img1.jpg"),
+  layout: {
+    flexDirection: "row-reverse", // Default layout direction
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  titleStyle: {
+    textAlign: "right",
+    marginRight: "20px",
+  },
+  descriptionStyle: {
+    textAlign: "right",
+    marginRight: "20px",
+  },
+  imageStyle: {
+    width: "300px",
+    height: "auto",
+    boxShadow: "1px 2px 35px rgba(0, 0, 0, 0.3)",
+  },
+  imageDirection: "right", // Default image direction
+};
+
+
+
+export const config2 = {
+  title: "Another Config",
+  description: "Another configuration example without an image.",
+  showImage: true,
+  imageUrl: require("../assets/images/img2.jpg"),
+  layout: {
+    flexDirection: "row", // Default layout direction
+    justifyContent: "space-between",
+    alignItems: "center",
+  },
+  titleStyle: {
+    textAlign: "left",
+    marginLeft: "20px",
+  },
+  descriptionStyle: {
+    textAlign: "left",
+    marginLeft: "20px",
+  },
+  imageStyle: {
+    margin: "40px",
+    width: "300px",
+    height: "auto",
+    justifyContent: "right",
+    boxShadow: "1px 2px 35px rgba(0, 0, 0, 0.3)",
+  },
+  imageDirection: "left", // Default image direction
+};
+
+
+export const config3 = {
+    title: "Config Driven Component",
+    description:
+      "This is a configurable component body. and god the Linde is a leading global industrial gases and engineering company with 2022 sales of $33 billion. We live our mission of making our world more productive every day by providin",
+    showImage: true,
+    imageUrl: require("../assets/images/img1.jpg"),
+    layout: {
+      flexDirection: "row-reverse", // Default layout direction
+      justifyContent: "space-between",
+      alignItems: "center",
+    },
+    titleStyle: {
+      textAlign: "right",
+      marginRight: "20px",
+    },
+    descriptionStyle: {
+      textAlign: "right",
+      marginRight: "20px",
+    },
+    imageStyle: {
+      width: "300px",
+      height: "auto",
+      boxShadow: "1px 2px 35px rgba(0, 0, 0, 0.3)",
+    },
+    imageDirection: "right", // Default image direction
+  };
