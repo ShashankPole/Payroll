a6d284f3c61c1b397c28e9542072b192b2940579
shravanijoshi987 <shravanijoshi987@gmail.com>
2023-08-24 16:07:38 +0530

    header section update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import Home from "./Pages/home/Home"
 import FooterComponent from "./footer/FooterComponent"
 import {config1, config2, config3 } from "./configurations/componentConfig"
+import Header from "./Header/Header";
+import Data from "./Header/Data.json"
 
 function App() {
   return (
@@ -9,6 +11,7 @@ function App() {
       <Home {...config2} /> 
       <Home {...config3} />
       <FooterComponent />
+      <Header data={Data} />
      
     </>
   );
