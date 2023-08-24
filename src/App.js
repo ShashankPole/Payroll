5a29d42b3f17660a2dcf53ffabbfe068d50481c6
ShashankPole <pole.shashank@infokalash.com>
2023-08-24 16:48:37 +0530

    app updated

--- src/App.js
+++ src/App.js
@@ -9,10 +9,10 @@ const homeConfig = [config1, config2, config3];
 function App() {
   return (
     <>
-    {homeConfig.map((config, index) => (
-      <Home key={index} {...config}/>
-    ))}
-      <FooterComponent />
+    
+      <Home />
+   
+     
       <Header data={Data} />
      
     </>
